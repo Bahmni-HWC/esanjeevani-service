21a837c07ce723a2fc4f0eb96ba043b058eccbd2
SanoferSameera <sanofersameera25@gmail.com>
2023-09-22 13:47:08 +0530

    374 | Add. empty spring application

---
+++ src/test/java/org/bahmnihwc/esanjeevaniservice/ESanjeevaniServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bahmnihwc.esanjeevaniservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESanjeevaniServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
